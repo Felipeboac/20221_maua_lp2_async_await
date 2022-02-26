346c46660420da8f779a1a1f14fca6d2568e6cdd
Felipe Boacnin <felipeboac@gmail.com>
2022-02-26 12:36:01 -0300

    Tentativa de prestar atenção

---
+++ Aula02.js
@@ -0,0 +1,11 @@
+console.log(1==1)
+console.log(1=='1')
+console.log(1 == [1, 1])
+console.log(null == undefined)
+console.log([] == false)
+console.log(typeof(Number([1, 1])))
+console.log([] == [])
+console.log([] === [])
+const a = []
+const b = a
+console.log("abc" === "abc")
